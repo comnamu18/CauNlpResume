7e60039d43a0b24d0fb57b2beb99e45bc3139490
dispe1 <lkjim0757@naver.com>
2019-06-06 12:36:53 +0900

    extract related words

--- dictionary/한국어기초사전/ExtractRelatedWords.py
+++ dictionary/한국어기초사전/ExtractRelatedWords.py
@@ -16,10 +16,9 @@ for p in pos:
         for row in sheet.rows:
             if row[3].value == p and t in row[4].value:
                 word = row[4].value.split(t +' ')
-                if len(word) == 2:
-                    ws.cell(row=row_index, column=1, value = row[0].value)
-                    ws.cell(row=row_index, column=2, value = word[1].split('참고어')[0].split('반대말')[0].split('준말')[0].split('센말')[0].split('높임말')[0].split('유의어')[0])
-                    row_index += 1
+                ws.cell(row=row_index, column=1, value = row[0].value)
+                ws.cell(row=row_index, column=2, value = word[len(word)-1].split('참고어')[0].split('반대말')[0].split('준말')[0].split('센말')[0].split('높임말')[0].split('유의어')[0])
+                row_index += 1
 
         wb.save("./" + p + "_" + t + ".xlsx")
         wb.close()
