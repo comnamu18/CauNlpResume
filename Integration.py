58de3b5233b73a71bd992e52d29ab8fd3e6ed5c2
Kim SangHeon <comnamu18@gmail.com>
2019-06-07 12:37:25 +0900

    Change File Type
    ANSI -> UTF-8

--- Integration.py
+++ Integration.py
@@ -1,24 +1,28 @@
 import os
 import pandas as pd
-from konlpy.tag import Mecab
+#from konlpy.tag import Mecab
 from collections import Counter
-from customNlp import mecab
+#from customNlp import mecab
 
 #일반명사, 고유명사, 동사, 형용사, 일반 부사
 TAG_LIST = ["NNG", "NNP", "VV", "VA", "MAG"]
 RECOMMEND_LIST = ["높임말", "유의어", "준말"]
 
 #Read dictionary csv and extract data
-data = pd.read_csv("./crawling/corpus/secresumecsv.csv")
+data = pd.read_csv("./crawling/corpus/secresumecsv.csv", encoding="utf-8")
 recommend_dict = data.loc[data['형태소'].isin(TAG_LIST)]
 
 #Input File
 INPUT_FILE = input("PLEASE GIVE RESUME : ")
-f = open(INPUT_FILE, 'r')
-input_text = f.read()
-f.close()
+try:
+    f = open(INPUT_FILE, 'r')
+    input_text = f.read()
+    f.close()
+except FileNotFoundError:
+    exit();
 
-tags = mecab.get_tags(input_text)
-print(tags)
+
+#tags = mecab.get_tags(input_text)
+#print(tags)
 # ToDo : Taglist에 있는 것들로만 뽑아내기
 
